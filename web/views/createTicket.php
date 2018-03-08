cc1d445f71323254ed69ac1fd0b85dc3e3aec6d1
Joël Schmid <joel.schmid@ffhs.ch>
2018-03-08 19:45:15 +0100

    Adding bootstrap

--- web/views/createTicket.php
+++ web/views/createTicket.php
@@ -3,6 +3,9 @@
 
 <head>
   <title>Page Title</title>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
 </head>
 
 <body>
