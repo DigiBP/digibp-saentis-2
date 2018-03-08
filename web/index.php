fc99d5e5b5f057e3328bd888b3a64f364dd35a8d
Joël Schmid <joel.schmid@ffhs.ch>
2018-03-08 16:12:46 +0100

    Fix routing

--- web/index.php
+++ web/index.php
@@ -19,7 +19,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+  return $app['twig']->render('index.html');
 });
 
 $app->run();
