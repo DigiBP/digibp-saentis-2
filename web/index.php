aa3b2192b447e741e38617e3e61e11f0df56a2b6
Ramirez Jaime <Jaime.Ramirez2@swisscom.com>
2018-03-15 13:29:36 +0100

    New CSS Test

--- web/index.php
+++ web/index.php
@@ -24,96 +24,22 @@
 <!-- Form Name -->
 <legend>Incident Creation</legend>
 
-<!-- Select Basic -->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="impact">Impcat</label>
-  <div class="col-md-2">
-    <select id="impact" name="impact" class="form-control">
-      <option value="1">1 - Critical</option>
-      <option value="2">2 - High</option>
-      <option value="3">3 - Medium</option>
-      <option value="4">4 - Low</option>
-    </select>
+<div class="wrap">
+  <h1>Rise</h1>
+  <h3>I built something similar as an engineer on Identity.com</h3>
+  <div>
+    <label for="fname">First Name</label>
+    <input id="fname" type="text" class="cool"/>
   </div>
-</div>
 
-<!-- Select Basic -->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="urgency">Urgency</label>
-  <div class="col-md-2">
-    <select id="urgency" name="urgency" class="form-control">
-      <option value="1">1 - High</option>
-      <option value="2">2 - Medium</option>
-      <option value="3">3 - Low</option>
-    </select>
+  <div>
+    <label for="lname">Last Name</label>
+    <input id="lname" type="text" class="cool"/>
   </div>
-</div>
-
-<!-- Select Basic -->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="priority">Priority</label>
-  <div class="col-md-2">
-    <select id="priority" name="priority" class="form-control">
-      <option value="1">1 - High</option>
-      <option value="2">2 - Medium</option>
-      <option value="3">3 - Low</option>
-    </select>
-  </div>
-</div>
-<!-- Select Basic -->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="affectedApplication">Affected Application</label>
-  <div class="col-md-2">
-    <select id="affectedApplication" name="affectedApplication" class="form-control">
-      <option value="1">Excel</option>
-      <option value="2">Word</option>
-      <option value="3">Lotus Notes</option>
-    </select>
-  </div>
-</div>
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="textinput">Description of the issue</label>  
-  <div class="col-md-5">
-  <input id="textinput" name="textinput" type="text" placeholder="Please desscribe your issue here." class="form-control input-md">
-    
-  </div>
-</div>
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="summary">Summary</label>  
-  <div class="col-md-5">
-  <input id="summary" name="summary" type="text" placeholder="Please desscribe your issue here." class="form-control input-md" required="">
-    
-  </div>
-</div>
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="SystemID">System ID</label>  
-  <div class="col-md-5">
-  <input id="SystemID" name="SystemID" type="text" placeholder="Indication on which System the user is." class="form-control input-md">
-    
-  </div>
-</div>
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="stepByStep">Provide detailed step-by-step Description</label>  
-  <div class="col-md-5">
-  <input id="stepByStep" name="stepByStep" type="text" placeholder="Provide a step-by-step Guide." class="form-control input-md">
-    
-  </div>
-</div>
-
-<!-- Text input-->
-<div class="form-group">
-  <label class="col-md-4 control-label" for="customerMail">E-Mail</label>  
-  <div class="col-md-5">
-  <input id="customerMail" name="customerMail" type="text" placeholder="Enter your E-Mail adress." class="form-control input-md" required="">
-    
+  
+  <div>
+    <label for="email">Some Long Copy Goes Here</label>
+    <input id="email" type="text" class="cool"/>
   </div>
 </div>
 
