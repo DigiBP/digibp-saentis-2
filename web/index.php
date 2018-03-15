733b90afd19dbf5a33b2cc43de794cba905694fe
Ramirez Jaime <Jaime.Ramirez2@swisscom.com>
2018-03-15 14:29:28 +0100

    new css new html

--- web/index.php
+++ web/index.php
@@ -20,39 +20,38 @@
     
     <form class="form-horizontal">
 
-<!--Rahmen ür Input  -->
-<div class="wrap">
-
-
-  <div>
-    <label for="fname">First Name</label>
-    <input id="fname" type="text" class="cool"/>
-  </div>
-    
-      
-  <div> 
-      <label for="impact">Impact</label>
-    <select class="cool" id="impact" type="text">
-      <option value="1">1 - Critical</option>
-      <option value="2">2 - High</option>
-      <option value="3">3 - Medium</option>
-      <option value="4">4 - Low</option>
-    </select>
-    
-  </div>
-
-
-    
-  <div>
-    <label for="lname">Last Name</label>
-    <input id="lname" type="text" class="cool"/>
-  </div>
-  
-  <div>
-    <label for="email">Enter your EMail adress here</label>
-    <input id="email" type="text" class="cool"/>
-  </div>
-    </div>
+<div class="form-style-3">
+  <form>
+    <fieldset>
+      <legend>Personal</legend>
+      <label for="field1"><span>Name <span class="required">*</span></span>
+        <input class="input-field" type="text" name="field1" value=""/>
+      </label>
+      <label for="field2"><span>Email <span class="required">*</span></span>
+        <input class="input-field" type="email" name="field2" value=""/>
+      </label>
+      <label for="field3"><span>Phone <span class="required">*</span></span>
+        <input class="input-field" type="text" name="field3" value=""/>
+      </label>
+      <label for="field4"><span>Subject</span>
+        <select class="select-field" name="field4">
+          <option value="Appointment">Appointment</option>
+          <option value="Interview">Interview</option>
+          <option value="Regarding a post">Regarding a post</option>
+        </select>
+      </label>
+    </fieldset>
+    <fieldset>
+      <legend>Message</legend>
+      <label for="field6"><span>Message <span class="required">*</span></span>
+        <textarea class="textarea-field" name="field6"></textarea>
+      </label>
+      <label><span> </span>
+        <input type="submit" value="Submit"/>
+      </label>
+    </fieldset>
+  </form>
+</div>
     
 
 </form>
