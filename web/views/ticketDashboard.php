61d2b3ca5b1fc4145e07251df942e2c3521fa016
Joël Schmid <joel.schmid@ffhs.ch>
2018-05-11 10:49:38 +0200

    Adding favicon

--- web/views/ticketDashboard.php
+++ web/views/ticketDashboard.php
@@ -5,7 +5,12 @@
   <title>Säntis - Dashboard</title>
   <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
- 
+ <link rel="apple-touch-icon" sizes="180x180" href="../images/apple-touch-icon.png">
+ <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
+ <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png">
+ <link rel="manifest" href="/site.webmanifest">
+ <meta name="msapplication-TileColor" content="#da532c">
+ <meta name="theme-color" content="#ffffff">
 
 </head>
 
